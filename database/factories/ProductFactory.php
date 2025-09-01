a91c3b84ba9da203d8671cb34f57717d438a55cc
Michell Henrick <michell.rv@hotmail.com.br>
2025-08-31 22:05:26 -0300

    development: creating the crud from product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,21 +3,25 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
  */
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Product::class;
+
     public function definition(): array
     {
+        // Forçar Faker em português
+        $this->faker = \Faker\Factory::create('pt_BR');
+
         return [
-            //
+            'name' => $this->faker->words(mt_rand(1, 3), true), // 1 a 3 palavras
+            'price' => $this->faker->randomFloat(2, 5, 500), // preço entre 5 e 500
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 Route::get('/ping', fn() => response()->json(['message' => 'pong']));
 
-Route::get('/products', fn() => response()->json(['message' => 'Product Index']))
-     ->name('product.index');
+/**
+ * GET /products
+ * GET /products/{id}
+ * POST /products
+ * PUT/PATCH /products/{id}
+ * DELETE /products/{id}
+ */
+
+Route::get('/products', fn(Product $product) => $product->all());
+Route::get('/products/{product}', fn(Product $product) => $product);
